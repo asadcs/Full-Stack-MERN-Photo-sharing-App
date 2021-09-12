107452b7b99e2830d4974a1775f4581e43d11981
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-09-12 02:16:07 -0700

    initial commit
    Initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+app.use("/post", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://asadcs:asadpscc54@cluster0.xx3v2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+console.log("asad");
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => console.warn("connected")
+    // app.listen(PORT, () =>
+    //   console.log(`Server Running on Port: http://localhost:${PORT}`)
+  );
+
+//.catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set("useFindAndModify", false);
+
+app.listen(PORT, () =>
+  console.log(`Server Running on Port: http://localhost:${PORT}`)
+);
