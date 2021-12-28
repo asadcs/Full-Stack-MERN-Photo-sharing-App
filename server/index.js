391ee972c7d93bea3cd688b6d89286ed9033d6ec
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-12-28 22:04:58 +0500

    crud completed
    crud completed

--- server/index.js
+++ server/index.js
@@ -3,22 +3,30 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  // "mongodb+srv://asadcs:asadpscc54@cluster0.xx3v2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-  "mongodb+srv://asadcs:asadpscc54@cluster0.1vvkv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.get("/", (req, res) => {
+  res.send("hello memories API");
+});
+//const CONNECTION_URL =
+// "mongodb+srv://asadcs:asadpscc54@cluster0.xx3v2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// "mongodb+srv://asadcs:asadpscc54@cluster0.1vvkv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 console.log("asad");
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.warn("connected");
     app.listen(PORT, () =>
